58408c973345aee98ecb820769b6fd4a99fde36c
dreampig <2328288177@qq.com>
2024-02-21 09:16:07 +0800

    线程池部分结束

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -19,6 +19,13 @@ private:
     pthread_t pthread_t *m_threads;//线程数组，用来存放线程
     locker m_queuelocker;//互斥访问请求队列；
     std::list<T *> m_workqueue; //请求队列
+    sem m_queuestat;            //是否有任务需要处理
+
+private:
+    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
+    static void *worker(void *arg);
+    void run();
+
 public:
     //thread_number是线程池中线程的数量
     //max_requests是请求队列中最多允许的、等待处理的请求的数量
@@ -68,3 +75,87 @@ threadpool<T>::~threadpool()
 {
     delete[] m_threads;
 }
+
+//插入请求队列
+template <typename T>
+bool threadpool<T>::append_p(T *request) {
+
+    //上锁
+    m_queuelocker.lock();
+
+    //判断队列是否满了
+    if(m_workqueue.size() >= m_max_requestes) {
+        m_queuelocker.unlock();
+        return false;
+    }
+
+    //插入队列
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+
+    //v操作
+    m_queuestat.post();
+    return true;
+
+}
+
+template <typename T>
+bool threadpool<T>::append_p(T *request) {
+
+    //上锁
+    m_queuelocker.lock();
+
+    //判断队列是否满了
+    if(m_workqueue.size() >= m_max_requestes) {
+        m_queuelocker.unlock();
+        return false;
+    }
+
+    //插入队列
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+
+    //v操作
+    m_queuestat.post();
+    return true;
+
+}
+
+template <typename T>
+bool threadpool<T>::append(T *request, int state)
+{
+    m_queuelocker.lock();
+    
+    //根据硬件，预先设置请求队列的最大值
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    
+    //添加任务
+    request->m_state = state;
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    
+     //信号量提醒有任务要处理
+    m_queuestat.post();
+    return true;
+}
+
+template <typename T>
+void *threadpool<T>::worker(void *arg)
+{
+	//将参数强转为线程池类，调用成员方法
+    threadpool *pool = (threadpool *)arg;
+    pool->run();
+    return pool;
+}
+
+//主要实现，工作线程从请求队列中取出某个任务进行处理，注意线程同步。
+template <typename T>
+void threadpool<T>::run()
+{
+    //request是啥？
+}
+#endif
